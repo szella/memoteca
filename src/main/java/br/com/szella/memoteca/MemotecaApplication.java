faa997d121145c0e67603acad84d206269e26f84
Zella <sergioluiz.zella@live.com>
2023-07-11 15:57:33 -0300

    primeiro commit

---
+++ src/main/java/br/com/szella/memoteca/MemotecaApplication.java
@@ -0,0 +1,13 @@
+package br.com.szella.memoteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/szella/memoteca/MemotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.szella.memoteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
